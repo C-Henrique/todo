61f89f0743e0dd504af77addc091d24901c5067a
Camilo Henrique <camilo.henrique@pecicero.com.br>
2024-06-26 08:23:41 -0300

    feat: delete todo

--- src/component/todo/ListTodosComponent.jsx
+++ src/component/todo/ListTodosComponent.jsx
@@ -1,55 +1,88 @@
-import { useEffect, useState } from "react"
-import { retrieveAllTodosForUsername } from "./api/TodosApiService"
+import { useEffect, useState } from "react";
+import {
+  deleteTodoApi,
+  retrieveAllTodosForUsernameApi,
+} from "./api/TodosApiService";
 
 function ListTodosComponent() {
+  const [todos, setTodos] = useState([]);
 
-    const today = new Date()
+  const [message, setMessage] = useState(null);
+  
+  const [showAlert, setShowAlert] = useState(false);
 
-    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
+  useEffect(() => refreshTodos(), []);
+  function refreshTodos() {
+    retrieveAllTodosForUsernameApi("in28minutes")
+      .then((resp) => setTodos(resp.data))
+      .catch((error) => console.error(error))
+      .finally(console.log("finish"));
+  }
 
-    const [todos, setTodos] = useState([])
-    useEffect(
-        () => refreshTodos(), []
-    )
-    function refreshTodos() {
+  function deleteTodo(id) {
+    deleteTodoApi("in28minutes", id)
+      .then(() => {
+          setMessage(`Todo do id ${id} removida com sucesso.`);
+          setShowAlert(true)
+        refreshTodos();
+      })
+      .catch((error) => console.error(error))
+      .finally(console.log("finish"));
+  }
+  function closedAlert() {
+    setShowAlert(false)
+  }
 
-        retrieveAllTodosForUsername('in28minutes').then( resp => setTodos(resp.data)).catch(error => console.error(error)).finally(console.log('finish'))
-    }
-
-
-
-    return (
-        <div className="container">
-            <h1>Things You Want To Do!</h1>
-            <div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <td>ID</td>
-                            <td>Description</td>
-                            <td>Is Done?</td>
-                            <td>Target Date</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            todos.map(
-                                todo => (
-                                    <tr key={todo.id}>
-                                        <td>{todo.id}</td>
-                                        <td>{todo.description}</td>
-                                        <td>{todo.done.toString()}</td>
-                                        <td>{todo.targetDate.toString()}</td>
-                                    </tr>
-                                )
-                            )
-                        }
-                    </tbody>
-
-                </table>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h1>Things You Want To Do!</h1>
+      <div>
+        {showAlert && (
+          <div
+            className="alert alert-warning alert-dismissible fade show"
+            role="alert"
+          >
+            {message}
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+              onClick={closedAlert}
+            ></button>
+          </div>
+        )}
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Is Done?</th>
+              <th>Target Date</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr key={todo.id}>
+                <td>{todo.description}</td>
+                <td>{todo.done.toString()}</td>
+                <td>{todo.targetDate.toString()}</td>
+                <td>
+                  <button
+                    type="button"
+                    class="btn btn-outline-warning"
+                    onClick={() => deleteTodo(todo.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default ListTodosComponent
+export default ListTodosComponent;
--- src/component/todo/api/TodosApiService.js
+++ src/component/todo/api/TodosApiService.js
@@ -5,4 +5,5 @@ const apiClient = axios.create({
 })
 
 
-export const retrieveAllTodosForUsername = (username) => apiClient.get(`/users/${username}/todos`)
+export const retrieveAllTodosForUsernameApi = (username) => apiClient.get(`/users/${username}/todos`)
+export const deleteTodoApi = (username, id) => apiClient.delete(`/users/${username}/todos/${id}`)
